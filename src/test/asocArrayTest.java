0b09db3287301cf30e544ef227dfcfd7c196d32f
josew383 <josew383@gmail.com>
2017-05-20 01:29:31 +0200

    refactor: añado #Before y #After

--- src/test/asocArrayTest.java
+++ src/test/asocArrayTest.java
@@ -2,21 +2,34 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import asocArray.AsocArray;
 
 public class asocArrayTest {
+	private AsocArray array;
+	
+	@Before
+	public void before(){
+		array = new AsocArray();
+		
+	}
+	
+	@After
+	public void after(){
+		
+		
+	}
 
 	@Test
 	public void creacionDelArray() {
-		AsocArray array = new AsocArray();
 		assertEquals(0, array.size());
 	}
 	
 	@Test
 	public void anadoValoresYClaves(){
-		AsocArray array = new AsocArray();
 		array.put("nombre","luis");
 		array.put("apellidos","martin");
 		assertEquals("luis", array.get("nombre"));
